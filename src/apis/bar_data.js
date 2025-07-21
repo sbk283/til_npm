ca771a8b73b797eed77fd063266cec3515375443
devgreact <dev.greact@gmail.com>
2025-07-21 15:10:48 +0900

    [docs] Nivo 차트 활용하기

---
+++ src/apis/bar_data.js
@@ -0,0 +1,18 @@
+export const barData = [
+  {
+    date: "2025-07-07",
+    point: 62,
+  },
+  {
+    date: "2025-07-08",
+    point: 32,
+  },
+  {
+    date: "2025-07-09",
+    point: 72,
+  },
+  {
+    date: "2025-07-10",
+    point: 22,
+  },
+];
